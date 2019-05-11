16dcb5e70c0e8cc924b4fa239158b11b0b29df77
Oleg Oseev <ooseev@yahoo.com>
2019-05-11 05:51:04 -0700

    add some

--- palindromepermutation/src/palindrome_permutation/PalindromePermutation.java
+++ palindromepermutation/src/palindrome_permutation/PalindromePermutation.java
@@ -1,6 +1,7 @@
 package palindrome_permutation;
 
-
+import java.util.HashSet;
+import java.util.Set;
 /**
  * 
  * Given a string, write a function to check if it is a permutation of a palindrome
@@ -9,15 +10,32 @@ public class PalindromePermutation {
 
 	public static void main(String[] args) {
 		
-		String s1 = "taco cat";
 		String s2 = "atco cta";
+		
+		
+		System.out.println("String '" + s2 + "' is permutation? " + isPalindromPermutationHash(s2));
 	}
 
-	static boolean isPalPerHash(String s1, String s2) {
-		
+	static boolean isPalindromPermutationHash(String str) {
+		Set<Character> set = new HashSet<>();
+
+		int len = str.length();
+		for (int i = 0; i < len; i++) {
+			char c = str.charAt(i);
+			if (c == ' ') continue;
+
+			if (set.contains(Character.valueOf(c))) {
+				set.remove(Character.valueOf(c));
+			} else {
+				set.add(Character.valueOf(c));
+			}
+		}
+
+		return set.size() == 0 || set.size() == 1;
 	}
 	
-	static boolean isPalPerBits(String s1, String s2) {
-		
+	static boolean isPalindromPermutationBit(String str) {
+		int len = str.length();
+		return false;
 	}
 }
